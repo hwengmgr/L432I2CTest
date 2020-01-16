9e649ac29d6296a896e0d3d0d5cfcf4b958ffbf3
hwengmgr <kleinl@yahoo.com>
2020-01-15 20:45:11 -0500

    initial commit of L432 version of program

---
+++ Core/Inc/sps30_example_usage.h
@@ -0,0 +1,13 @@
+/*
+ * sps30_example_usage.h
+ *
+ *  Created on: Jan 12, 2020
+ *      Author: klein
+ */
+
+#ifndef INC_SPS30_EXAMPLE_USAGE_H_
+#define INC_SPS30_EXAMPLE_USAGE_H_
+
+int sps30_example_usage(void);
+
+#endif /* INC_SPS30_EXAMPLE_USAGE_H_ */
